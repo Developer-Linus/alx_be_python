d259d9ad6324c31fefe7d09608c3aaec782acd27
Developer-Linus <linusmksu30@gmail.com>
2024-12-11 16:12:16 +0300

    Completed basic Python operations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,16 @@
+#Define variables
+number1 = 10
+number2 = 5
+# Sum of number1 and number2
+sum = number1 + number2
+
+# Difference between the two numbers
+subtraction = number1 - number2
+
+# Product of number1 and number2
+product = number1 * number2
+
+# Print the output of the operations to the console
+print(f"Addition of {number1} and {number2} is {sum}")
+print(f"Subtraction of {number1} and {number2} is {subtraction}")
+print(f"Multiplication of {number1} and {number2} is {product}")
