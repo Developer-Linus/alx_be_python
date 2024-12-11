b7879276b52174dfccc5c5ba3c312f64a4bcf75b
Developer-Linus <linusmksu30@gmail.com>
2024-12-11 16:49:54 +0300

    Script to calculate the future age of user based on current age

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+# This program prompts user for current age and calculates how old the user will be in future
+
+# Define the variable by asking the user 
+current_age = int(input("How old are you?"))
+
+# Calculate how old the user will be in 2050 assuming current year is 2023
+age = current_age + 27
+
+# Print the output to the console
+print(f"In 2050, you will be {age} years old.")
--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -7,4 +7,4 @@ hours = 2
 seconds = hours * 3600
 
 # Print the result to the console
-print(f"{hours} hour(s) is {seconds} seconds")
+print(f"{hours} hour(s) is {seconds} seconds.")
