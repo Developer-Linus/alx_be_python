8934ed1dd21ad407d61b6121a3a009899d3745ee
Developer-Linus <linusmksu30@gmail.com>
2024-12-20 17:00:15 +0300

    updated weather_advice.py file

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,7 +1,6 @@
 # Prompting the user of the current weather and giving advice based on the weather
-
 current_weather = input("What's the weather like today? (sunny/rainy/cold):")
-
+# Recommendations based on user's input
 if current_weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
 elif current_weather == "rainy":
