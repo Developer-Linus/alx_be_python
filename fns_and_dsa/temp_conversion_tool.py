6adbce2ab1b05dbf06f4f62085020a6001e67612
Developer-Linus <linusmksu30@gmail.com>
2025-01-10 17:57:41 +0300

    added temperature conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -2,14 +2,16 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
 
-temp_input = float(input("Enter the temperature to convert: "))
-temp_state = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
+
+
 
 # Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
+        temp_input = float(input("Enter the temperature to convert: "))
+        temp_state = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
         fahrenheit = temp_input
-        if fahrenheit:
-                temp_celsius = FAHRENHEIT_TO_CELSIUS_FACTOR*(fahrenheit - 32)
+        if fahrenheit and temp_state == "F":
+                temp_celsius = (fahrenheit - 32)*FAHRENHEIT_TO_CELSIUS_FACTOR
                 print(f"{fahrenheit}F is {temp_celsius}°C")
         else:
                 print("Invalid temperature. Please enter a numeric value.")
@@ -17,19 +19,14 @@ def convert_to_celsius(fahrenheit):
     
 # Function to convert Celsius to Fahrenheit
 def convert_to_fahrenheit(celsius):
+        temp_input = float(input("Enter the temperature to convert: "))
+        temp_state = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").strip().upper()
         celsius = temp_input
-        if celsius:
-                temp_fahrenheit = ((celsius*CELSIUS_TO_FAHRENHEIT_FACTOR) + 32)
+        if celsius and temp_state == "C":
+                temp_fahrenheit = (32 + (celsius*CELSIUS_TO_FAHRENHEIT_FACTOR))
                 print(f"{celsius}°C is {temp_fahrenheit}F")
         else:
                 print("Invalid temperature. Please enter a numeric value.")
-
-if temp_state == "F":
-        convert_to_celsius(fahrenheit = temp_input)
-elif temp_state == "C":
-        convert_to_fahrenheit(celsius=temp_input)
-else:
-        print("Invalide temperature unit.")
         
         
 
