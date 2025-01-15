2db894906318b80deeef5e31d6df0d6ee65c26b3
Developer-Linus <linusmksu30@gmail.com>
2025-01-15 13:55:35 +0300

    completed task 0

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,21 @@
+# Define class
+class BankAccount:
+
+    def __init__(self, account_balance):
+        self.account_balance = account_balance
+        self.initial_balance = 0
+
+    def deposit(self, amount):
+        self.account_balance += amount
+    def withdraw(self, amount):
+        if amount > self.account_balance:
+            self.account_balance -= amount
+        else:
+            print("The account balance is insufficient to withdraw {amount}.")
+    def display_balance(self):
+        print("Current balance: {self.account_balance}")
+
+    
+# Instantiating a class
+account = BankAccount(200)
+    
